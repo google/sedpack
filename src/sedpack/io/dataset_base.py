62c1b771be885286288f6ec37b7f78f8d037aaca
Karel Král <karelkral@google.com>
2025-08-06 22:08:39 +0200

    Allow arbitrary shard iteration
    Start migration to allow customizing shard info iteration.

--- src/sedpack/io/dataset_base.py
+++ src/sedpack/io/dataset_base.py
@@ -13,11 +13,13 @@
 # limitations under the License.
 """Base class for a dataset."""
 import itertools
+import json
 import logging
 from pathlib import Path
+import random
 import semver
 
-from typing import Iterator, Union
+from typing import Callable, Iterator, Union
 
 import sedpack
 from sedpack.io.shard_file_metadata import ShardInfo, ShardsList, ShardListInfo
@@ -162,10 +164,13 @@ class ShardInfoIterator:
     """Iterate shards of a dataset.
     """
 
-    def __init__(self,
-                 split: SplitT | None,
-                 dataset: DatasetBase,
-                 repeat: bool = False) -> None:
+    def __init__(
+        self,
+        *,
+        split: SplitT | None,
+        dataset: DatasetBase,
+        repeat: bool = False,
+    ) -> None:
         """Initialize shard information iteration.
 
         Args:
@@ -235,3 +240,117 @@ class ShardInfoIterator:
         """Get the next item.
         """
         return next(self._iterator)
+
+
+class CachedShardInfoIterator(ShardInfoIterator):
+    """Iterate shards of a dataset.
+    """
+
+    def __init__(
+        self,
+        *,
+        split: SplitT | None,
+        dataset: DatasetBase,
+        repeat: bool = False,
+        shards: int | None = None,
+        custom_metadata_type_limit: int | None = None,
+        shard_filter: Callable[[ShardInfo], bool] | None = None,
+        shuffle: int = 0,
+    ) -> None:
+        """Initialize shard information iteration.
+
+        Args:
+
+          split (SplitT | None): Which split to iterate or all if set to None.
+
+          dataset (DatasetBase): The dataset being iterated.
+
+          repeat (bool): Should we cycle indefinitely?
+
+          shards (int | None): If specified limits the dataset to the first
+          `shards` shards.
+
+          custom_metadata_type_limit (int | None): Ignored when None. If
+          non-zero then limit the number of shards with different
+          `custom_metadata`. Take only the first `custom_metadata_type_limit`
+          shards with the concrete `custom_metadata`. This is best effort for
+          different `custom_metadata` (string of sorted items).
+
+          shard_filter (Callable[[ShardInfo], bool | None): If present this is
+          a function taking the ShardInfo and returning True if the shard shall
+          be used for traversal and False otherwise.
+
+          shuffle (int): When set to 0 the iteration is deterministic.
+        """
+        super().__init__(
+            split=split,
+            dataset=dataset,
+            repeat=repeat,
+        )
+
+        self.shuffle: int = shuffle
+
+        # Cache the list of shards.
+        shard_list: list[ShardInfo] = list(
+            ShardInfoIterator(
+                split=split,
+                dataset=dataset,
+                repeat=False,
+            ))
+
+        # Filter if needed.
+        if shard_filter:
+            shard_list = [
+                shard_info for shard_info in shard_list
+                if shard_filter(shard_info)
+            ]
+
+        # Only use a limited amount of shards for each setting of
+        # custom_metadata.
+        if custom_metadata_type_limit:
+            counts: dict[str, int] = {}
+            old_shards_list = shard_list
+            shard_list = []
+            for shard_info in old_shards_list:
+                k: str = str(tuple(sorted(shard_info.custom_metadata.items())))
+                counts[k] = counts.get(k, 0) + 1
+                if counts[k] <= custom_metadata_type_limit:
+                    shard_list.append(shard_info)
+
+        # Limit the number of shards.
+        if shards:
+            shard_list = shard_list[:shards]
+
+        # Initial shuffling.
+        if shuffle:
+            random.shuffle(shard_list)
+
+        # Cached shards.
+        self._index: int = -1  # The last returned element.
+        self._shards: list[ShardInfo] = shard_list
+
+    def number_of_shards(self) -> int:
+        """Return the number of distinct shards that are iterated. When
+        repeated this method still returns a finite answer.
+        """
+        return len(self._shards)
+
+    def __iter__(self) -> Iterator[ShardInfo]:
+        """Return the shard information iterator (reentrant).
+        """
+        return self
+
+    def __next__(self) -> ShardInfo:
+        """Get the next item.
+        """
+        self._index += 1
+
+        if self._index >= len(self._shards):
+            if self.repeat:
+                self._index = 0
+                if self.shuffle:
+                    random.shuffle(self._shards)
+            else:
+                raise StopIteration
+
+        return self._shards[self._index]
