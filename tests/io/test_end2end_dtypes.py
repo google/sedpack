3b86d4dfcd2f5037ba2e4616cc7ff10f6d5a5ff5
Karel Král <karelkral@google.com>
2025-08-28 14:47:32 +0200

    [squash] parametrized tests
    - not all of these pass
    - large int is still turned into a NumPy value

--- tests/io/test_end2end_dtypes.py
+++ tests/io/test_end2end_dtypes.py
@@ -13,7 +13,8 @@
 # limitations under the License.
 
 from pathlib import Path
-from typing import Union
+from typing import Any
+import random
 
 import numpy as np
 import numpy.typing as npt
@@ -26,32 +27,54 @@ from sedpack.io import Metadata
 from sedpack.io.types import TRAIN_SPLIT, CompressionT, ShardFileTypeT
 
 
-def end2end_str(
-    tmpdir: Union[str, Path],
-    method: str,
-    shard_file_type: ShardFileTypeT,
-    compression: CompressionT,
-) -> None:
-    array_of_values = [
-        "https://arxiv.org/abs/2306.07249",
-        "Ḽơᶉëᶆ ȋṕšᶙṁ ḍỡḽǭᵳ ʂǐť ӓṁệẗ, ĉṓɲṩḙċťᶒțûɾ",
-        "ấɖḯƥĭṩčįɳġ ḝłįʈ, șếᶑ ᶁⱺ ẽḭŭŝḿꝋď ṫĕᶆᶈṓɍ ỉñḉīḑȋᵭṵńť ṷŧ ḹẩḇőꝛế",
-        "éȶ đꝍꞎôꝛȇ ᵯáꞡᶇā ąⱡîɋṹẵ.",
-    ]
-
-    tiny_experiment_path: Path = Path(tmpdir) / "e2e_str_experiment"
-
-    # Create a dataset
-
+def dataset_and_values_dynamic_shape(
+    tmpdir: str | Path,
+    shard_file_type: str,
+    compression: str,
+    dtypes: list[str],
+    items: int,
+) -> (Dataset, dict[str, list[Any]]):
+    values: dict[str, list[Any]] = {}
+    ds_path = Path(tmpdir) / f"e2e_{shard_file_type}_{'_'.join(dtypes)}"
     dataset_metadata = Metadata(description="Test of the lib")
+
+    # The order should not play a role.
+    random.shuffle(dtypes)
 
     example_attributes = [
         sedpack.io.metadata.Attribute(
-            name="strange_strings",
-            dtype="str",
+            name=f"attribute_{dtype}",
+            dtype=dtype,
             shape=(),
-        ),
+        ) for dtype in dtypes
     ]
+
+    for dtype in dtypes:
+        values[f"attribute_{dtype}"] = []
+
+        match dtype:
+            case "int":
+                for _ in range(items):
+                    # TODO larger range than just int64
+                    values[f"attribute_{dtype}"].append(
+                        random.randint(-60**2, 60**2))
+            case "str":
+                long_string = "Ḽơᶉëᶆ ȋṕšᶙṁ ḍỡḽǭᵳ ʂǐť ӓṁệẗ, ĉṓɲṩḙċťᶒțûɾ" \
+                    "https://arxiv.org/abs/2306.07249 ḹẩḇőꝛế" \
+                    "ấɖḯƥĭṩčįɳġ ḝłįʈ, șếᶑ ᶁⱺ ẽḭŭŝḿꝋď ṫĕᶆᶈṓɍ ỉñḉīḑȋᵭṵńť ṷŧ" \
+                    ":(){ :|:& };: éȶ đꝍꞎôꝛȇ ᵯáꞡᶇā ąⱡîɋṹẵ."
+                for _ in range(items):
+                    begin: int = random.randint(0, len(long_string) // 2)
+                    end: int = random.randint(begin + 1, len(long_string))
+                    values[f"attribute_{dtype}"].append(long_string[begin:end])
+            case "bytes":
+                for _ in range(items):
+                    values[f"attribute_{dtype}"].append(
+                        np.random.randint(
+                            0,
+                            256,
+                            size=random.randint(5, 20),
+                        ).tobytes())
 
     dataset_structure = sedpack.io.metadata.DatasetStructure(
         saved_data_description=example_attributes,
@@ -66,7 +89,7 @@ def end2end_str(
             attribute_name=attribute.name) == attribute
 
     dataset = Dataset.create(
-        path=tiny_experiment_path,
+        path=ds_path,
         metadata=dataset_metadata,
         dataset_structure=dataset_structure,
     )
@@ -74,17 +97,126 @@ def end2end_str(
     # Fill data in the dataset
 
     with dataset.filler() as filler:
-        for attribute_value in array_of_values:
+        for i in range(items):
             filler.write_example(
-                values={"strange_strings": attribute_value},
+                values={
+                    name: value[i] for name, value in values.items()
+                },
                 split=TRAIN_SPLIT,
             )
 
     # Check the data is correct
     # Reopen the dataset
-    dataset = Dataset(tiny_experiment_path)
+    dataset = Dataset(ds_path)
     dataset.check()
 
+    return (values, dataset)
+
+
+@pytest.fixture(
+    scope="module",
+    params=[
+        {
+            "dtypes": ["str"],
+            "compression": "GZIP",
+        },
+        #{
+        #    "dtypes": ["bytes"],
+        #    "compression": "GZIP",
+        #},
+        #{
+        #    "dtypes": ["int"],
+        #    "compression": "GZIP",
+        #},
+        #{
+        #    "dtypes": ["str", "bytes", "int"],
+        #    "compression": "GZIP",
+        #},
+    ],
+)
+def values_and_dataset_tfrec(request, tmpdir_factory) -> None:
+    shard_file_type: str = "tfrec"
+    yield dataset_and_values_dynamic_shape(
+        tmpdir=tmpdir_factory.mktemp(f"dtype_{shard_file_type}"),
+        shard_file_type=shard_file_type,
+        compression=request.param["compression"],
+        dtypes=request.param["dtypes"],
+        items=137,
+    )
+    # Teardown.
+
+
+@pytest.fixture(
+    scope="module",
+    params=[
+        {
+            "dtypes": ["str"],
+            "compression": "ZIP",
+        },
+        {
+            "dtypes": ["bytes"],
+            "compression": "ZIP",
+        },
+        #{
+        #    "dtypes": ["int"],
+        #    "compression": "ZIP",
+        #},
+        #{
+        #    "dtypes": ["str", "bytes", "int"],
+        #    "compression": "ZIP",
+        #},
+    ],
+)
+def values_and_dataset_npz(request, tmpdir_factory) -> None:
+    shard_file_type: str = "npz"
+    yield dataset_and_values_dynamic_shape(
+        tmpdir=tmpdir_factory.mktemp(f"dtype_{shard_file_type}"),
+        shard_file_type=shard_file_type,
+        compression=request.param["compression"],
+        dtypes=request.param["dtypes"],
+        items=137,
+    )
+    # Teardown.
+
+
+@pytest.fixture(
+    scope="module",
+    params=[
+        {
+            "dtypes": ["str"],
+            "compression": "LZ4",
+        },
+        #{
+        #    "dtypes": ["bytes"],
+        #    "compression": "LZ4",
+        #},
+        #{
+        #    "dtypes": ["int"],
+        #    "compression": "LZ4",
+        #},
+        #{
+        #    "dtypes": ["str", "bytes", "int"],
+        #    "compression": "LZ4",
+        #},
+    ],
+)
+def values_and_dataset_fb(request, tmpdir_factory) -> None:
+    shard_file_type: str = "fb"
+    yield dataset_and_values_dynamic_shape(
+        tmpdir=tmpdir_factory.mktemp(f"dtype_{shard_file_type}"),
+        shard_file_type=shard_file_type,
+        compression=request.param["compression"],
+        dtypes=request.param["dtypes"],
+        items=137,
+    )
+    # Teardown.
+
+
+def check_iteration_of_values(
+    method: str,
+    dataset: Dataset,
+    values: dict[str, list[Any]],
+) -> None:
     match method:
         case "as_tfdataset":
             for i, example in enumerate(
@@ -94,12 +226,14 @@ def end2end_str(
                         repeat=False,
                         batch_size=1,
                     )):
-                # No idea how to have an actual string in TensorFlow. Maybe it
-                # is best to leave it as a tensor anyway since that is the
-                # "native" type.
-                #assert type(example["strange_strings"][0]) == type(
-                #    array_of_values[i])
-                assert example["strange_strings"] == array_of_values[i:i + 1]
+                assert len(example) == len(values)
+
+                # No idea how to have an actual string or bytes in TensorFlow.
+                # Maybe it is best to leave it as a tensor anyway since that is
+                # the "native" type.
+
+                for name, returned_batch in example.items():
+                    assert returned_batch == values[name][i:i + 1]
         case "as_numpy_iterator":
             for i, example in enumerate(
                     dataset.as_numpy_iterator(
@@ -107,9 +241,10 @@ def end2end_str(
                         shuffle=0,
                         repeat=False,
                     )):
-                assert type(example["strange_strings"]) == type(
-                    array_of_values[i])
-                assert example["strange_strings"] == array_of_values[i]
+                assert len(example) == len(values)
+                for name, returned_value in example.items():
+                    assert type(returned_value) == type(values[name][i])
+                    assert returned_value == values[name][i]
         case "as_numpy_iterator_concurrent":
             for i, example in enumerate(
                     dataset.as_numpy_iterator_concurrent(
@@ -117,12 +252,25 @@ def end2end_str(
                         shuffle=0,
                         repeat=False,
                     )):
-                assert type(example["strange_strings"]) == type(
-                    array_of_values[i])
-                assert example["strange_strings"] == array_of_values[i]
+                assert len(example) == len(values)
+                for name, returned_value in example.items():
+                    assert type(returned_value) == type(values[name][i])
+                    assert returned_value == values[name][i]
+        case "as_numpy_iterator_rust":
+            for i, example in enumerate(
+                    dataset.as_numpy_iterator_concurrent(
+                        split=TRAIN_SPLIT,
+                        shuffle=0,
+                        repeat=False,
+                    )):
+                assert len(example) == len(values)
+                for name, returned_value in example.items():
+                    assert type(returned_value) == type(values[name][i])
+                    assert returned_value == values[name][i]
 
     # We tested everything
-    assert i + 1 == len(array_of_values), "Not all examples have been iterated"
+    if i + 1 != len(next(iter(values.values()))):
+        raise AssertionError("Not all examples have been iterated")
 
     # Number of shards matches
     full_iterator = ShardInfoIterator(
@@ -141,7 +289,6 @@ def end2end_str(
         ).number_of_shards() for split in ["train", "test", "holdout"])
 
 
-# TODO common fixture tfrec_dataset
 @pytest.mark.parametrize("method", [
     "as_tfdataset",
     "as_numpy_iterator",
@@ -149,45 +296,44 @@ def end2end_str(
 ])
 def test_end2end_dtypes_str_tfrec(
     method: str,
-    tmpdir: Union[str, Path],
+    values_and_dataset_tfrec,
 ) -> None:
-    end2end_str(
-        tmpdir=tmpdir,
+    values, dataset = values_and_dataset_tfrec
+    check_iteration_of_values(
         method=method,
-        shard_file_type="tfrec",
-        compression="GZIP",
+        dataset=dataset,
+        values=values,
     )
 
 
-# TODO common fixture npz_dataset
 @pytest.mark.parametrize("method", [
     "as_numpy_iterator",
     "as_numpy_iterator_concurrent",
 ])
 def test_end2end_dtypes_str_npz(
     method: str,
-    tmpdir: Union[str, Path],
+    values_and_dataset_npz,
 ) -> None:
-    end2end_str(
-        tmpdir=tmpdir,
+    values, dataset = values_and_dataset_npz
+    check_iteration_of_values(
         method=method,
-        shard_file_type="npz",
-        compression="ZIP",
+        dataset=dataset,
+        values=values,
     )
 
 
-# TODO common fixture fb_dataset
 @pytest.mark.parametrize("method", [
     "as_numpy_iterator",
     "as_numpy_iterator_concurrent",
+    "as_numpy_iterator_rust",
 ])
 def test_end2end_dtypes_str_fb(
     method: str,
-    tmpdir: Union[str, Path],
+    values_and_dataset_fb,
 ) -> None:
-    end2end_str(
-        tmpdir=tmpdir,
+    values, dataset = values_and_dataset_fb
+    check_iteration_of_values(
         method=method,
-        shard_file_type="fb",
-        compression="LZ4",
+        dataset=dataset,
+        values=values,
     )
