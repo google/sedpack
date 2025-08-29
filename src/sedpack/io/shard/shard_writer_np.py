f8d2ff89878f03398258f98e4bd73be48c3c4c10
Karel Král <karelkral@google.com>
2025-08-29 13:21:53 +0200

    [squash] nit allow pickle

--- src/sedpack/io/shard/shard_writer_np.py
+++ src/sedpack/io/shard/shard_writer_np.py
@@ -143,15 +143,11 @@ class ShardWriterNP(ShardWriterBase):
             case "ZIP":
                 np.savez_compressed(
                     str(self._shard_file),
-                    # See comment above.
-                    #allow_pickle=False,
                     **self._buffer,  # type: ignore[arg-type]
                 )
             case "":
                 np.savez(
                     str(self._shard_file),
-                    # See comment above.
-                    #allow_pickle=False,
                     **self._buffer,  # type: ignore[arg-type]
                 )
             case _:
